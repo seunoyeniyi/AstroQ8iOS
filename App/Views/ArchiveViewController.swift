cd7375400c15d6e6b6d63d184c009a2a10de087c
Seun Oyeniyi <iamseunoyeniyi@gmail.com>
2022-03-06 10:36:52 -0800

    commit

--- App/Views/ArchiveViewController.swift
+++ App/Views/ArchiveViewController.swift
@@ -321,10 +321,16 @@ extension ArchiveViewController: UICollectionViewDataSource, UICollectionViewDel
         } else { //single product
             cell.productPrice.text = Site.init().CURRENCY + PriceFormatter.format(price: self.products[indexPath.row].getPrice())
             
-            let regularPriceAtt: NSMutableAttributedString = NSMutableAttributedString(string: Site.init().CURRENCY + PriceFormatter.format(price: self.products[indexPath.row].getRegularPrice()))
-            regularPriceAtt.addAttribute(NSAttributedStringKey.strikethroughStyle, value: 2, range: NSMakeRange(0, regularPriceAtt.length))
-            cell.regularPrice.attributedText = regularPriceAtt
-            cell.regularPrice.isHidden = false
+            if (self.products[indexPath.row].getRegularPrice() != self.products[indexPath.row].getPrice()) {
+                let regularPriceAtt: NSMutableAttributedString = NSMutableAttributedString(string: Site.init().CURRENCY + PriceFormatter.format(price: self.products[indexPath.row].getRegularPrice()))
+                regularPriceAtt.addAttribute(NSAttributedStringKey.strikethroughStyle, value: 2, range: NSMakeRange(0, regularPriceAtt.length))
+                cell.regularPrice.attributedText = regularPriceAtt
+                cell.regularPrice.isHidden = false
+            } else {
+                cell.regularPrice.isHidden = true
+            }
+            
+            
         }
         //wishlist button
         if (cell.hasWishList) {
